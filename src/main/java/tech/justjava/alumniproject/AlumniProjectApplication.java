936186da752fd54ef37bc547e4adecd1f7e12889
mrpelumi <oguntolapelumi6@gmail.com>
2025-11-04 08:23:04 +0100

    Initial commit: Spring Boot starter auto-generated by Agentic AI

---
+++ src/main/java/tech/justjava/alumniproject/AlumniProjectApplication.java
@@ -0,0 +1,13 @@
+package tech.justjava.alumniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlumniProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/justjava/alumniproject/AlumniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.justjava.alumniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
